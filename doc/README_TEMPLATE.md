dcc9b43007c33dcb9b76a3ff3ae2bbf8e5277939
Dmitry Trager <dmitry.trager@flatstack.com>
2017-10-27 12:55:45 +0300

    Update elixir version to 1.5

--- doc/README_TEMPLATE.md
+++ doc/README_TEMPLATE.md
@@ -12,7 +12,7 @@ Some short project description. Link to Basecamp project will be fine too.
 
 ## Dependencies
 
-* Elixir 1.3
+* Elixir 1.5
 
 Setup required dependencies from `Brewfile`:
 ```bash
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixBase.Mixfile do
   def project do
     [app: :phoenix_base,
      version: "0.0.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
