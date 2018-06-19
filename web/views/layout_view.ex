2d79b559ba36c991be728611d4a22067f04ea7d7
Dmitry Trager <dmitry.trager@flatstack.com>
2018-06-19 19:23:39 +0300

    Make ujs delete optionable for sign out link

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule PhoenixBase.LayoutView do
   use PhoenixBase.Web, :view
+
+  def sign_out_options do
+    if Mix.env == :test do
+      []
+    else
+      ["ujs-method": "delete"]
+    end
+  end
 end
